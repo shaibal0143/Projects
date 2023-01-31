3e9f696c8b3850b8100bbc3a2f3a0ca55b992f21
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2023-01-31 09:21:48 +0530

    Update Tax_Calculator.java

--- Tax_Calculator.java
+++ Tax_Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Tax_Calculator {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your Income::"); 
         double n = scan.nextDouble();
